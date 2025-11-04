51d7ebf0ba88175fae21db7dd28d49856096beab
Deepthi <deepthireddy20200@gmail.com>
2025-11-04 21:17:51 +0530

    Fresh start - working React app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Hello, I'm Akhil!</h1>
+      <p>This is my first React app (now with Vite!).</p>
+      <button style={{ padding: "10px 20px", fontSize: "16px" }}>
+        Click Me
+      </button>
+    </div>
+  );
+}
+export default App;
